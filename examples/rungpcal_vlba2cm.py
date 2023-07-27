b57011de4f0e72314666388eae76eea2a42f91db
jhparkastro <jpark@asiaa.sinica.edu.tw>
2023-07-27 16:13:38 +0900

    updates

--- examples/rungpcal_vlba2cm.py
+++ examples/rungpcal_vlba2cm.py
@@ -21,23 +21,13 @@ direc = '/home/jpark/gpcaltest/vlba_2cm/'
 dataname = 'bl229ae.u.edt.'
 
 # The list of calibrators which will be used for an initial D-term estimation using the similarity assumption.
-calsour = ['0256+075', '0851+202', '2200+420', '2201+315', '2258-022']
+calsour = ['0851+202', '2258-022']
 
 # The list of the number of CLEAN sub-models for calsour.
-cnum = [3, 3, 5, 2, 3]
+cnum = [3, 3]
 
 # The list of booleans specifying whether the sub-model division will be done automatically or manually.
-autoccedt = [False, False, True, False, False]
-
-
-# The list of calibrators which will be used for an initial D-term estimation using the similarity assumption.
-calsour = ['0851+202']
-
-# The list of the number of CLEAN sub-models for calsour.
-cnum = [3]
-
-# The list of booleans specifying whether the sub-model division will be done automatically or manually.
-autoccedt = [False]
+autoccedt = [False, False]
 
 
 # Perform instrumental polarization self-calibraiton.
@@ -89,25 +79,11 @@ dplot_IFsep = True
 # Output the figures in the format of png.
 filetype = 'png'
 
-
-# The real and imaginary parts of all D-terms are assumed to be within 50%.
-Dbound = 0.5
-
-# The source-polarization terms for the D-term estimation using the similarity assumption are assumed to be less than 500%. 
-Pbound = 5.
-
-
 # The D-term plots will be shown for ranges of (-15%, 15%) for both the real and imaginary parts.
 drange = 15.
 
 
 time1 = timeit.default_timer()
-
-# Load the GPCAL class POLCAL using the above parameters.
-# obs = gp.polcal(aips_userno, direc, dataname, calsour, source, cnum, autoccedt, Dbound = Dbound, Pbound = Pbound, \
-#                solint = solint, solmode = solmode, soltype = soltype, weightit = weightit, dplot_IFsep = dplot_IFsep, \
-#                drange = drange, polcalsour = polcalsour, ms = ms, ps = ps, uvbin = uvbin, uvpower = uvpower, dynam = dynam, selfpoliter = selfpoliter, \
-#                selfcal=selfcal, vplot=vplot, resplot=resplot, parplot = parplot, selfpol=selfpol, filetype = filetype)
 
 obs = gp.gpcal.polcal(aips_userno, direc, dataname, calsour, source, cnum, autoccedt, polcalsour = polcalsour, ms = ms, ps = ps, uvbin = uvbin, uvpower = uvpower, dynam = dynam, selfpoliter = selfpoliter, \
                       dplot_IFsep = dplot_IFsep, selfcal=selfcal, vplot=vplot, resplot=resplot, parplot = parplot, selfpol=selfpol, filetype = filetype, multiproc = multiproc, nproc = nproc, vplot_scanavg = True)
